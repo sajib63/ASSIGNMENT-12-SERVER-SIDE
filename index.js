3de6cef81af27510df76ce844b28512a0dd1d516
Sajib Ahmed <sajib7315@gmail.com>
2022-11-24 23:13:59 +0600

    ading mongodb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const { json } = require('express')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port =process.env.PORT || 5000
 
 
@@ -13,6 +14,12 @@ app.use(express.json())
 
 
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.z2qhqgi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
